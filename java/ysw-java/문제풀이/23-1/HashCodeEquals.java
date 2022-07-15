a96328f8a62bcb8f59a413bb9a900c5197bfe3c6
Heejin <huren091@naver.com>
2022-07-16 02:32:34 +0900

    23-1 correction

--- java/ysw-java/문제풀이/23-1/HashCodeEquals.java
+++ java/ysw-java/문제풀이/23-1/HashCodeEquals.java
@@ -4,12 +4,13 @@ import java.util.Objects;
 public class HashCodeEquals {
 	public static void main(String[] args) {
 		HashSet<Person> set = new HashSet<>();
-		set.add(new Person("14", "abc"));
-		set.add(new Person("21", "asd"));
-		set.add(new Person("25", "zxc"));
-		set.add(new Person("21", "asd"));
+		set.add(new Person("hj",14));
+		set.add(new Person("mj",21));
+		set.add(new Person("yd",15));
+		set.add(new Person("mj",21));
 		
-		System.out.println("인스턴스 수 : "+ set.size());
+		System.out.println("instance amount "+ set.size());
+		System.out.println();
 		
 		for (Person person : set) {
 			System.out.println(person.toString() + "\t");
@@ -20,8 +21,9 @@ public class HashCodeEquals {
 
 class Person{
 	private String name;
-	private String age;
-	public Person(String name, String age) {
+	private int age;
+	
+	public Person(String name,int age) {
 		this.name = name;
 		this.age = age;
 
@@ -29,20 +31,20 @@ class Person{
 	
 	@Override
 	public String toString() {
-		return name + "(" + age + "세)";
+		return name + " " + "age: " + age;
 	}
 	
+	@Override
 	public int hashCode() {
 	return Objects.hash(name, age);
 	}
 	
-	
 	@Override 
 	public boolean equals(Object obj) {
-		Person n = (Person)obj).name;
-		Person a = (Person)obj).age;
+		String n = ((Person)obj).name;
+		int a = ((Person)obj).age;
 		
-		if(name.equals(n) && age.equals(a)) 
+		if(name.equals(n) && (age==a)) 
 			return true;
 		else
 			return false;
